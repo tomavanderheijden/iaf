b6dee8f504b3da127cbd33b650e22c7e65d3bf5a
Laurens Mäkel <laurens.makel@nn-group.com>
2020-01-07 14:25:14 +0100

    Remove un used imports

--- core/src/main/java/nl/nn/adapterframework/webcontrol/api/ShowDocumentation.java
+++ core/src/main/java/nl/nn/adapterframework/webcontrol/api/ShowDocumentation.java
@@ -20,12 +20,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
-import javax.servlet.ServletConfig;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.xml.transform.Source;
